00dc3c5535b6224dc6a9536bd2bbe819cd18c855
vinodreddy3003 <138984613+vinodreddy3003@users.noreply.github.com>
2024-10-05 23:16:15 +0530

    Update Films.jsx

--- src/components/Films.jsx
+++ src/components/Films.jsx
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect } from 'react';
 import './films.css';
-import videoFile1 from '../assets/Films/film1.mp4';
-import videoFile2 from '../assets/Films/film2.mp4';
-import videoFile3 from '../assets/Films/film3.mp4';
-import videoFile4 from '../assets/Films/film4.mp4';
+// import videoFile1 from '../assets/Films/film1.mp4';
+// import videoFile2 from '../assets/Films/film2.mp4';
+// import videoFile3 from '../assets/Films/film3.mp4';
+// import videoFile4 from '../assets/Films/film4.mp4';
 
 // Import posters for the videos
 import poster1 from '../assets/Films/Gayathri_20240929_181637_0000.png';
@@ -11,7 +11,7 @@ import poster2 from '../assets/Films/Gayathri & Pramod_20240929_172114_0000.png'
 import poster3 from '../assets/Films/Gayathri & Pramod_20240929_174109_0000.png';
 import poster4 from '../assets/Films/Gayathri & Pramod_20240929_173935_0000.png';
 
-const videos = [videoFile1, videoFile2, videoFile3, videoFile4]; // Array of videos
+// const videos = [videoFile1, videoFile2, videoFile3, videoFile4]; // Array of videos
 const posters = [poster1, poster2, poster3, poster4]; // Array of posters
 
 const Films = () => {
