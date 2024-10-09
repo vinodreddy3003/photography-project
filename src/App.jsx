7f02f3da2188fe2676e114d87cabe8e99e79cb64
vinodreddy3003 <138984613+vinodreddy3003@users.noreply.github.com>
2024-10-09 18:16:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"; // Assuming Navbar is in components folder
+import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 import AboutMe from "./components/AboutMe";
 import Contact from "./components/Contact";
 import Portfolio from "./components/Portfolio";
-import PreWedding from "./components/PreWedding"; // Component for pre-wedding
+import PreWedding from "./components/PreWedding";
 import Modeling from "./components/Modeling";
-// import Films from "./components/Films";
 import Birthday from "./components/Birthday";
 import Wedding from "./components/Wedding";
 import Reception from "./components/Reception";
+import ScrollToTop from "./ScrollToTop";
+import "./App.css";
+// Import the ScrollToTop component
 
 function App() {
   return (
     <>
       <Navbar />
+      < ScrollToTop /> {/* Place the ScrollToTop component here */}
       <div className="content">
         <Routes>
-          
           <Route path="/" element={<Homepage />} />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/wedding" element={<Wedding />} /> {/* Fixed path for wedding shoot */}
+          <Route path="/wedding" element={<Wedding />} />
           <Route path="/prewedding" element={<PreWedding />} />
-          <Route path="/modeling" element={< Modeling />} />
+          <Route path="/modeling" element={<Modeling />} />
           <Route path="/birthday" element={<Birthday />} />
-{/*           <Route path="/films" element={<Films />} /> */}
           <Route path="/reception" element={<Reception />} />
-   
-          <Route path="/contact" element={<Contact />} /> 
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </>
