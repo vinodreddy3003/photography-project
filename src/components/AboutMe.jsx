4983cf65a9aa6e8911feaf621d3bc89db75a0117
vinodreddy3003 <138984613+vinodreddy3003@users.noreply.github.com>
2024-10-17 16:15:04 +0530

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -18,13 +18,13 @@ const AboutMe = () => {
           <div className="text-section">
             <h2>ABOUT US</h2>
             <p>
-            From a young age, I discovered my passion for photography while exploring nature and capturing fleeting moments. Armed with my first camera, I found joy in framing the world around me, transforming everyday scenes into compelling stories. This passion deepened as I honed my skills through practice and experimentation, learning the art of lighting, composition, and post-processing.
+            From a young age, we discovered our shared passion for photography while exploring nature and capturing fleeting moments. Armed with our first camera, we found joy in framing the world around us, transforming everyday scenes into compelling stories. As we continued this journey together, our passion deepened, and we honed our skills through practice and experimentation, learning the art of lighting, composition, and post-processing.
 
-Over the years, I’ve had the privilege of achieving several milestones in my photography journey. My work has been featured in local exhibitions, and I’ve collaborated with various brands for promotional campaigns. Winning awards in photography contests has further validated my efforts and fueled my ambition to grow.
+Over the years, we’ve achieved several milestones in our photography journey. Our work has been featured in local exhibitions, and we’ve collaborated with various brands for promotional campaigns. Winning awards in photography contests has further validated our efforts and fueled our ambition to grow even more.
 
 
             </p>
-            <p>Looking ahead, my goals are to expand my portfolio by exploring new styles and subjects, particularly in portrait and documentary photography. I aspire to host my own exhibitions, share my knowledge through workshops, and connect with fellow photographers globally. Ultimately, I aim to use my photography to tell impactful stories and inspire others to see the beauty in everyday life.</p>
+            <p>Looking ahead, our goals are to expand our portfolio by exploring new styles and subjects, particularly in portrait and documentary photography. We aspire to host our own exhibitions, share our knowledge through workshops, and connect with fellow photographers globally. Ultimately, we aim to use our photography to tell impactful stories and inspire others to see the beauty in everyday life.</p>
           </div>
           <div className="image-section">
             <img src={portraitImage1} alt="Photographer 1" className="portrait-image" />
